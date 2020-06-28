7ece84965de3cd18e8491a0c04453e32ac0b5e43
Chris <csjones@users.noreply.github.com>
2020-06-27 23:57:48 -0700

    Cleaning up Package.swift

--- Package.swift
+++ Package.swift
@@ -11,55 +11,25 @@ let package = Package(
             name: "secp256k1",
             targets: ["secp256k1"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "secp256k1",
             path: "Sources/secp256k1",
             exclude: [
-                "asm",
-                "src/asm",
                 "secp256k1/src/asm",
-                "src/tests_exhaustive.c",
-                "secp256k1/src/tests_exhaustive.c",
-                "src/tests.c",
-                "secp256k1/src/tests.c",
-                "src/gen_context.c",
-                "secp256k1/src/gen_context.c",
-                "src/bench_ecdh.c",
                 "secp256k1/src/bench_ecdh.c",
-                "src/bench_ecmult.c",
                 "secp256k1/src/bench_ecmult.c",
-                "src/bench_internal.c",
                 "secp256k1/src/bench_internal.c",
-                "src/bench_recover.c",
                 "secp256k1/src/bench_recover.c",
-                "src/bench_sign.c",
                 "secp256k1/src/bench_sign.c",
-                "src/bench_verify.c",
                 "secp256k1/src/bench_verify.c",
-                "src/valgrind_ctime_test.c",
+                "secp256k1/src/gen_context.c",
+                "secp256k1/src/tests_exhaustive.c",
+                "secp256k1/src/tests.c",
                 "secp256k1/src/valgrind_ctime_test.c"
             ],
-//            sources: [
-//                "secp256k1/include",
-//                "secp256k1/contrib",
-//                "secp256k1/modules/ecdh",
-//                "secp256k1/modules/recovery",
-//                "secp256k1/src"
-//            ],
             cSettings: [
-//                .headerSearchPath(".."),
-//                .headerSearchPath("../.."),
-//                .headerSearchPath("../../.."),
                 .headerSearchPath("secp256k1"),
-                .headerSearchPath("secp256k1/src"),
-                .headerSearchPath("secp256k1/include"),
                 .define("USE_NUM_NONE"),
                 .define("USE_FIELD_INV_BUILTIN"),
                 .define("USE_SCALAR_INV_BUILTIN"),
